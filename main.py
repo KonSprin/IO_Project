49bf565d4ea24a72cb34c04bf15eae19af3835c1
JakubBekier <44197747+JakubBekier@users.noreply.github.com>
2020-12-08 15:11:09 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import cv2
 from GUICode import Ui_IO
 from video_processing import process
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -22,16 +23,17 @@ class MainWindow(QMainWindow):
         self.show()
 
         # SETTINGS / CONNECTIONS
-        self.ui.btnChooseVideo.clicked.connect(self.chooseVideo)      
+        self.ui.btnChooseVideo.clicked.connect(self.chooseVideo)
 
-        self.ui.btnChooseVideo_2.clicked.connect(self.chooseSaveFile)  
-        
+        self.ui.btnChooseVideo_2.clicked.connect(self.chooseSaveFile)
+
         self.ui.BtnStartprocess.clicked.connect(self.detect)
 
         self.ui.btnSaveLogs.clicked.connect(self.save_logs)
 
         self.ui.btnPlayVideo.clicked.connect(self.play)
 
+    # Set video path
     def chooseVideo(self):
         starting_directory = ''
         if platform.system() == "Windows":
@@ -39,11 +41,12 @@ class MainWindow(QMainWindow):
         else:
             starting_directory = 'HOME'
 
-        fname = QFileDialog.getOpenFileName(self, 'Choose Video', str(os.path.dirname(os.path.abspath(__file__))), "Video Files (*.mp4 *.mkv *.avi)")
+        fname = QFileDialog.getOpenFileName(self, 'Choose Video', str(os.path.dirname(os.path.abspath(__file__))),
+                                            "Video Files (*.mp4 *.mkv *.avi)")
         # SET lineEdit TO CHOSEN PATH
         self.ui.videoPath.setText(fname[0])
 
-
+    # Set logs path
     def chooseSaveFile(self):
         starting_directory = ''
         if platform.system() == "Windows":
@@ -52,14 +55,18 @@ class MainWindow(QMainWindow):
             starting_directory = 'HOME'
 
         # fname = QFileDialog.getOpenFileName(self, 'Choose Log File', os.environ[starting_directory])
-        fname = QFileDialog.getExistingDirectory(self, 'Choose Log File', str(os.path.dirname(os.path.abspath(__file__))))
+        fname = QFileDialog.getExistingDirectory(self, 'Choose Log File',
+                                                 str(os.path.dirname(os.path.abspath(__file__))))
         # SET lineEdit TO CHOSEN PATH
         self.ui.lineEdit_2.setText(fname)
 
+    # Analyze video
     def detect(self):
+        # Get paths
         fname = self.ui.videoPath.text()
         log_dir = self.ui.lineEdit_2.text()
         log_window = self.ui.plainTextEdit
+        # Check if paths are correct
         if os.path.isdir(log_dir) and os.path.isfile(fname):
             process(self.ui.progressBar, fname, log_dir, log_window)
         elif os.path.isdir(log_dir):
@@ -67,12 +74,16 @@ class MainWindow(QMainWindow):
         elif os.path.isfile(fname):
             ctypes.windll.user32.MessageBoxW(0, "Wybierz istniejący folder na logi", "Error", 1)
         else:
-            ctypes.windll.user32.MessageBoxW(0, "Wybierz istniejący folder na logi oraz poprawną ścieżkę do nagrania", "Error", 1)
+            ctypes.windll.user32.MessageBoxW(0, "Wybierz istniejący folder na logi oraz poprawną ścieżkę do nagrania",
+                                             "Error", 1)
 
+    # Save logs
     def save_logs(self):
+        # Get logs path and logs as a plain text
         logger = self.ui.plainTextEdit.toPlainText()
         INPUT_DIR = self.ui.videoPath.text()
-        LOG_DIR = self.ui.lineEdit_2.text() + "/" +INPUT_DIR.split('/')[-1].split('.')[0] + '.csv'
+        LOG_DIR = self.ui.lineEdit_2.text() + "/" + INPUT_DIR.split('/')[-1].split('.')[0] + '.csv'
+        # Convert logs into a list and save
         logger = ast.literal_eval(logger)
         with open(LOG_DIR, 'w', newline='') as f:
             writer = csv.writer(f, delimiter=',',
@@ -81,26 +92,29 @@ class MainWindow(QMainWindow):
             for car in logger:
                 writer.writerow(car)
 
+    # Play processed video
     def play(self):
+        # Get output path based on original video path
         input_file = self.ui.videoPath.text()
         output_file = input_file.split('.')[0] + '_output.' + input_file.split('.')[1]
         cap = cv2.VideoCapture(output_file)
         output_file = output_file.split("/")[-1]
+        # Play until video ends
         while (cap.isOpened()):
             ret, frame = cap.read()
             if not ret:
                 break
             frame = cv2.resize(frame, None, None, fx=0.5, fy=0.5)
             cv2.imshow(output_file, frame)
-            key = cv2.waitKey(int(1000/cap.get(cv2.CAP_PROP_FPS)))
-            
+            key = cv2.waitKey(int(1000 / cap.get(cv2.CAP_PROP_FPS)))
+
             if key in [27, 1048603]:  # ESC key to abort, close window
                 cv2.destroyAllWindows()
                 break
 
-
         cap.release()
         # cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
